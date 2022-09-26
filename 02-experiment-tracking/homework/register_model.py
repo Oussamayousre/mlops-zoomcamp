ba05147283cf380043403cb920118714c6cb801e
oussama <yousreoussama@gmail.com>
2022-09-26 16:50:54 +0100

    oussama first commit

--- 02-experiment-tracking/homework/register_model.py
+++ 02-experiment-tracking/homework/register_model.py
@@ -16,7 +16,7 @@ EXPERIMENT_NAME = "random-forest-best-models"
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment(EXPERIMENT_NAME)
 mlflow.sklearn.autolog()
-
+#gvjhgjgv
 SPACE = {
     'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
     'n_estimators': scope.int(hp.quniform('n_estimators', 10, 50, 1)),
